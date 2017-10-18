726210e8df21350595aca73d17dd5e5ca1429bf0
Joe Draper <jdraper@fastspring.com>
2017-10-18 13:57:18 -0700

    initial commit

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+  <?php echo '<p>Test Page</p>'; ?> 
+  <?php 
+    print_r($_REQUEST);
+  ?> 
+  <?php
+    phpinfo();
+   ?>
+ </body>
+</html>
+
